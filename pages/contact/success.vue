01d2964a2908095c410959eee987f59ae15c6700
Chris Wolf <cw.wolf@live.de>
2022-10-28 12:12:07 +0200

    add form

---
+++ pages/contact/success.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+    <section class="w-full max-w-5xl mx-auto">
+      <h1 class="title">Success</h1>
+      <p>
+        Form submit successful
+      </p>
+    </section>
+  </main>
+</template>
