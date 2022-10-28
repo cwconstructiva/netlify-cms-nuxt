067b409137e0cf5515978e7ca10340043615aacf
Chris Wolf <cw.wolf@live.de>
2022-10-28 11:57:55 +0200

    create contact page

---
+++ pages/contact/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+    <section class="w-full max-w-5xl mx-auto">
+      <h1 class="title">Contact</h1>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi, dignissimos necessitatibus consequatur quos
+      laborum sint at, perspiciatis voluptatem ad delectus quo! Eligendi soluta quo earum voluptas ducimus praesentium
+      sapiente eaque!
+    </section>
+  </main>
+</template>
